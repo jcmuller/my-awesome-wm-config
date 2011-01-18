d610107725b408c0e412736f5d5a4f043c14695e
Juan C. Muller <jcmuller@gmail.com>
2011-01-18 17:46:16 -0500

    Make global keys a private variable.

--- rc.lua
+++ rc.lua
@@ -774,7 +774,7 @@ root.buttons(awful.util.table.join(
 ))
 -- }}}
 -- {{{ Key bindings
-globalkeys = awful.util.table.join(
+local globalkeys = awful.util.table.join(
 	awful.key({ modkey,           }, "Left",   awful.tag.viewprev),
 	awful.key({ modkey,           }, "Right",  awful.tag.viewnext),
 	awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
